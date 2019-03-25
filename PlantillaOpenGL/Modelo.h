99fb4cde87302323677b8c97d65896578df5ee83
maria fernanda barraza rodriguez <reikafer@gmail.com>
2019-03-25 10:59:37 -0700

    Agregar archivos de proyecto.

---
+++ PlantillaOpenGL/Modelo.h
@@ -0,0 +1,35 @@
+#pragma once
+
+//--------------------------------------------------------Las clases van con la primera letra MAYUSCULA--------------------------------------------------------
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define GLEW_STATIC
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#include <vector>
+#include "Vertice.h"
+#include "Shader.h"
+
+using namespace std;
+
+
+class Modelo {
+
+public:
+
+	vector<Vertice> vertices;
+	Shader *shader;
+	GLuint vertexArrayID;
+	GLuint bufferID;
+
+
+	Modelo();
+
+	void inicializarVertexArray(GLuint posicionID, GLuint colorID);
+
+	void dibujar(GLenum modoDibujo);
+		
+};
